1115f5671daa7625ad44c5e06f0206cd6f05726d
parkyounghwan <dudrnxps1@gmail.com>
2021-05-07 02:57:44 +0900

    #1 프로젝트 생성

---
+++ src/main/kotlin/com/flabedu/blackpostoffice/ServletInitializer.kt
@@ -0,0 +1,12 @@
+package com.flabedu.blackpostoffice
+
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
+
+class ServletInitializer : SpringBootServletInitializer() {
+
+    override fun configure(application: SpringApplicationBuilder): SpringApplicationBuilder {
+        return application.sources(BlackPostofficeApplication::class.java)
+    }
+
+}
