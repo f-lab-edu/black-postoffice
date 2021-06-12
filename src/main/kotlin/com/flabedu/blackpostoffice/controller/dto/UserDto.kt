a97e24d58f404c66e328e559cec616a1ef4f4fa4
Hyung1Jung <junghyungile@gmail.com>
2021-06-13 01:36:53 +0900

    [#16] refactor(UserDto) : 생성자 대신 companion object 사용 취소

--- src/main/kotlin/com/flabedu/blackpostoffice/controller/dto/UserDto.kt
+++ src/main/kotlin/com/flabedu/blackpostoffice/controller/dto/UserDto.kt
@@ -8,7 +8,7 @@ import javax.validation.constraints.NotBlank
 import javax.validation.constraints.Pattern
 import javax.validation.constraints.Size
 
-data class UserDto private constructor(
+data class UserDto(
 
     @field:Email(message = "올바른 이메일 주소를 입력해주세요.")
     @field:NotBlank(message = "이메일은 필수 입력입니다.")
@@ -32,16 +32,6 @@ data class UserDto private constructor(
     val createdAt: LocalDateTime = LocalDateTime.now()
 
 ) {
-    companion object {
-        fun of(
-            email: String,
-            password: String,
-            nickName: String,
-            address: String,
-            phone: String,
-            createdAt: LocalDateTime
-        ) = UserDto(email, password, nickName, address, phone, createdAt)
-    }
 
     fun toUserEntity(password: String) =
         User(
