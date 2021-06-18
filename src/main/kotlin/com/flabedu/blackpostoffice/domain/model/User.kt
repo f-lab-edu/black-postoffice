d82de06d890bef780169a7ca05d9700e341970f5
Hyung1Jung <junghyungile@gmail.com>
2021-06-18 23:54:18 +0900

    [#16] refactor : private constructor() 제거

--- src/main/kotlin/com/flabedu/blackpostoffice/domain/model/User.kt
+++ src/main/kotlin/com/flabedu/blackpostoffice/domain/model/User.kt
@@ -3,23 +3,21 @@ package com.flabedu.blackpostoffice.domain.model
 import java.time.LocalDateTime
 
 class User(
-    val address: String,
+    val address: String = "",
 
     val createdAt: LocalDateTime = LocalDateTime.now(),
 
-    val email: String,
+    val email: String = "",
 
-    val nickName: String,
+    val nickName: String = "",
 
-    val password: String,
+    val password: String = "",
 
-    val phone: String,
+    val phone: String = "",
 
     val userRole: UserRole = UserRole.USER
 
 ) {
-    private constructor() : this(address = "", email = "", nickName = "", password = "", phone = "")
-
     enum class UserRole {
         NONE, USER, ADMIN
     }
