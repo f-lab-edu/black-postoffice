fd1bc60f550b6f8c62fdb408a4c93387594618f9
Hyung1Jung <junghyungile@gmail.com>
2021-06-22 00:18:37 +0900

    [#18] refactor : 클래스명 수정 UserRole -> Role

--- src/main/kotlin/com/flabedu/blackpostoffice/domain/model/User.kt
+++ src/main/kotlin/com/flabedu/blackpostoffice/domain/model/User.kt
@@ -3,15 +3,22 @@ package com.flabedu.blackpostoffice.domain.model
 import java.time.LocalDateTime
 
 class User(
-    val address: String,
+    val address: String = "",
 
-    val createdAt: LocalDateTime,
+    val createdAt: LocalDateTime = LocalDateTime.now(),
 
-    val email: String,
+    val email: String = "",
 
-    val nickName: String,
+    val nickName: String = "",
 
-    val password: String,
+    val password: String = "",
 
-    val phone: String
-)
+    val phone: String = "",
+
+    val role: Role = Role.USER
+
+) {
+    enum class Role {
+        NONE, USER, ADMIN
+    }
+}
