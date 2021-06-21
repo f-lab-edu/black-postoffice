16e5072a7ecfdbcba2aac8d0c9c103e467bb2712
Hyung1Jung <junghyungile@gmail.com>
2021-06-22 00:23:12 +0900

    [#18] refactor : Error response 형식 관련 수정 / body() -> JSON 형식

--- src/main/kotlin/com/flabedu/blackpostoffice/exception/user/AccessRejectedException.kt
+++ src/main/kotlin/com/flabedu/blackpostoffice/exception/user/AccessRejectedException.kt
@@ -1,3 +1,13 @@
 package com.flabedu.blackpostoffice.exception.user
 
-class AccessRejectedException : RuntimeException()
+import com.flabedu.blackpostoffice.util.logging.logger
+
+class AccessRejectedException : RuntimeException(MESSAGE) {
+    companion object {
+        private const val MESSAGE = "이 페이지에 엑세스 하는데 필요한 권한이 존재하지 않습니다."
+    }
+
+    init {
+        logger.debug(MESSAGE)
+    }
+}
--- src/main/kotlin/com/flabedu/blackpostoffice/exception/user/UserNotLoginException.kt
+++ src/main/kotlin/com/flabedu/blackpostoffice/exception/user/UserNotLoginException.kt
@@ -1,3 +1,13 @@
 package com.flabedu.blackpostoffice.exception.user
 
-class UserNotLoginException : RuntimeException()
+import com.flabedu.blackpostoffice.util.logging.logger
+
+class UserNotLoginException : RuntimeException(MESSAGE) {
+    companion object {
+        private const val MESSAGE = "로그인 후에 이용 가능합니다."
+    }
+
+    init {
+        logger.debug(MESSAGE)
+    }
+}
