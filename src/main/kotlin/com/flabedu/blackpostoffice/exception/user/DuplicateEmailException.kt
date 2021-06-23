21f844d8318baa45b57061c62ffe722959964b9f
Hyung1Jung <junghyungile@gmail.com>
2021-06-23 20:13:43 +0900

    [#18] refactor : ResponseConstants 클래스 생성하여 예외 메세지를 한 곳에서 관리

--- src/main/kotlin/com/flabedu/blackpostoffice/exception/user/DuplicateEmailException.kt
+++ src/main/kotlin/com/flabedu/blackpostoffice/exception/user/DuplicateEmailException.kt
@@ -1,13 +1,5 @@
 package com.flabedu.blackpostoffice.exception.user
 
-import com.flabedu.blackpostoffice.util.logging.logger
+import com.flabedu.blackpostoffice.commom.utils.constants.DUPLICATED_EMAIL
 
-class DuplicateEmailException : IllegalArgumentException(MESSAGE) {
-    companion object {
-        private const val MESSAGE = "이미 존재하는 이메일 입니다."
-    }
-
-    init {
-        logger.debug(MESSAGE)
-    }
-}
+class DuplicateEmailException : IllegalArgumentException(DUPLICATED_EMAIL)
