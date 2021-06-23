02703d181cb10cb3fb94e95be6b58f6d6a1c9c1d
Hyung1Jung <junghyungile@gmail.com>
2021-06-23 20:14:53 +0900

    [#18] refactor(UserExceptionHandler) : 각 예외 클래스들의 로그 메시지를 핸들러 클래스로 이동

--- src/main/kotlin/com/flabedu/blackpostoffice/exception/user/handler/UserExceptionHandler.kt
+++ src/main/kotlin/com/flabedu/blackpostoffice/exception/user/handler/UserExceptionHandler.kt
@@ -4,7 +4,10 @@ import com.flabedu.blackpostoffice.exception.dto.ErrorResponseDto
 import com.flabedu.blackpostoffice.exception.user.AccessRejectedException
 import com.flabedu.blackpostoffice.exception.user.DuplicateEmailException
 import com.flabedu.blackpostoffice.exception.user.UnauthorizedLoginException
-import com.flabedu.blackpostoffice.exception.user.UserNotLoginException
+import com.flabedu.blackpostoffice.commom.utils.constants.DUPLICATED_EMAIL
+import com.flabedu.blackpostoffice.commom.utils.constants.INVALID_LOGIN_INFO
+import com.flabedu.blackpostoffice.commom.utils.constants.NOT_AUTHORIZED
+import com.flabedu.blackpostoffice.commom.logging.logger
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
@@ -15,17 +18,22 @@ class UserExceptionHandler {
 
     @ResponseStatus(HttpStatus.CONFLICT)
     @ExceptionHandler(DuplicateEmailException::class)
-    fun handleDuplicateEmailException(exception: DuplicateEmailException) = ErrorResponseDto(exception.message!!)
+    fun handleDuplicateEmailException(exception: DuplicateEmailException): ErrorResponseDto {
+        logger.debug(DUPLICATED_EMAIL)
+        return ErrorResponseDto(exception.message!!)
+    }
 
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
     @ExceptionHandler(UnauthorizedLoginException::class)
-    fun handleEmailNotExistsException(exception: UnauthorizedLoginException) = ErrorResponseDto(exception.message!!)
-
-    @ResponseStatus(HttpStatus.UNAUTHORIZED)
-    @ExceptionHandler(UserNotLoginException::class)
-    fun handleUserNotLoginException(exception: UserNotLoginException) = ErrorResponseDto(exception.message!!)
+    fun handleUnauthorizedLoginException(exception: UnauthorizedLoginException): ErrorResponseDto {
+        logger.debug(INVALID_LOGIN_INFO)
+        return ErrorResponseDto(exception.message!!)
+    }
 
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
     @ExceptionHandler(AccessRejectedException::class)
-    fun handleAccessRejectedException(exception: AccessRejectedException) = ErrorResponseDto(exception.message!!)
+    fun handleAccessRejectedException(exception: AccessRejectedException): ErrorResponseDto {
+        logger.debug(NOT_AUTHORIZED)
+        return ErrorResponseDto(exception.message!!)
+    }
 }
