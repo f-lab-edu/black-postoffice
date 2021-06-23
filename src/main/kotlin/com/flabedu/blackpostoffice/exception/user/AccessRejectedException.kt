59f6ff71be33de26d96a202d2b2ffa50ebeb6398
Hyung1Jung <junghyungile@gmail.com>
2021-06-23 20:13:43 +0900

    [#18] refactor : ResponseConstants 클래스 생성하여 예외 메세지를 한 곳에서 관리

--- src/main/kotlin/com/flabedu/blackpostoffice/exception/user/AccessRejectedException.kt
+++ src/main/kotlin/com/flabedu/blackpostoffice/exception/user/AccessRejectedException.kt
@@ -1,13 +1,5 @@
 package com.flabedu.blackpostoffice.exception.user
 
-import com.flabedu.blackpostoffice.util.logging.logger
+import com.flabedu.blackpostoffice.commom.utils.constants.NOT_AUTHORIZED
 
-class AccessRejectedException : RuntimeException(MESSAGE) {
-    companion object {
-        private const val MESSAGE = "이 페이지에 엑세스 하는데 필요한 권한이 존재하지 않습니다."
-    }
-
-    init {
-        logger.debug(MESSAGE)
-    }
-}
+class AccessRejectedException : RuntimeException(NOT_AUTHORIZED)
--- src/main/kotlin/com/flabedu/blackpostoffice/exception/user/UnauthorizedLoginException.kt
+++ src/main/kotlin/com/flabedu/blackpostoffice/exception/user/UnauthorizedLoginException.kt
@@ -1,13 +1,5 @@
 package com.flabedu.blackpostoffice.exception.user
 
-import com.flabedu.blackpostoffice.util.logging.logger
+import com.flabedu.blackpostoffice.commom.utils.constants.INVALID_LOGIN_INFO
 
-class UnauthorizedLoginException : RuntimeException(MESSAGE) {
-    companion object {
-        private const val MESSAGE = "아이디가 존재하지 않거나 비밀번호가 일치하지 않습니다."
-    }
-
-    init {
-        logger.debug(MESSAGE)
-    }
-}
+class UnauthorizedLoginException : RuntimeException(INVALID_LOGIN_INFO)
