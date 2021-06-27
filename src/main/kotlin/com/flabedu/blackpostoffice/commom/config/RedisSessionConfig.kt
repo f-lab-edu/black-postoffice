44507fb088c87fbddf6c7f8f44477542974cf71f
Hyung1Jung <junghyungile@gmail.com>
2021-06-27 21:38:15 +0900

    [#23] style : 코드 포맷팅

--- src/main/kotlin/com/flabedu/blackpostoffice/commom/config/RedisSessionConfig.kt
+++ src/main/kotlin/com/flabedu/blackpostoffice/commom/config/RedisSessionConfig.kt
@@ -3,7 +3,6 @@ package com.flabedu.blackpostoffice.commom.config
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.data.redis.connection.RedisConnectionFactory
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
 import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer
@@ -18,9 +17,9 @@ class RedisConfig(
     private val redisHost: String,
 
     @Value("\${spring.redis.port}")
-    private val redisPort: Int,
+    private val redisPort: Int
 
-    ) {
+) {
 
     @Bean
     fun redisConnectionFactory() = LettuceConnectionFactory(redisHost, redisPort)
