f02e93a770beaad26122cd717598e4eae907b185
Hyung1Jung <junghyungile@gmail.com>
2021-06-30 20:56:30 +0900

    [#18] refactor : 클래스 추적 가능한 Logger Interface 추가

--- src/main/kotlin/com/flabedu/blackpostoffice/commom/utils/constants/Logger.kt
+++ src/main/kotlin/com/flabedu/blackpostoffice/commom/utils/constants/Logger.kt
@@ -3,4 +3,7 @@ package com.flabedu.blackpostoffice.commom.utils.constants
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
 
-val logger: Logger = LogManager.getLogger()
+interface Logger {
+    val logger: Logger
+        get() = LogManager.getLogger(this)
+}
