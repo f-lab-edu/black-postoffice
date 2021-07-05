aa9c5a633c50b693c2a9a5c0f5f004e184030a25
Hyung1Jung <junghyungile@gmail.com>
2021-07-06 00:48:26 +0900

    [#21] remove : 불필요한 파일 삭제

--- src/main/kotlin/com/flabedu/blackpostoffice/commom/property/AmazonS3Property.kt
+++
@@ -1,8 +0,0 @@
-// package com.flabedu.blackpostoffice.commom.property
-//
-// import org.springframework.boot.context.properties.ConfigurationProperties
-// import org.springframework.boot.context.properties.ConstructorBinding
-//
-// @ConstructorBinding
-// @ConfigurationProperties(prefix = "spring.cloud.aws")
-// class AmazonS3Property(val accessKey: String, secretKey: String, region: String)
