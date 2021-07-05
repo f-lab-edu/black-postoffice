74aa512a0264a3c50bd6a957a11aa067fb028d21
Hyung1Jung <junghyungile@gmail.com>
2021-07-06 01:04:29 +0900

    [#18] refactor : logger 관련 리팩토링

--- src/main/kotlin/com/flabedu/blackpostoffice/commom/logging/Logger.kt
+++
@@ -1,9 +0,0 @@
-package com.flabedu.blackpostoffice.commom.logging
-
-import org.apache.logging.log4j.LogManager
-import org.apache.logging.log4j.Logger
-
-interface Logger {
-    val logger: Logger
-        get() = LogManager.getLogger(this)
-}
