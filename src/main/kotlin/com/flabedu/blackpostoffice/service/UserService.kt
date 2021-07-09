63e1539091f01e171cfa39b0246e2acf1ede6fa5
Hyung1Jung <junghyungile@gmail.com>
2021-07-10 03:59:18 +0900

    [#31] refactor : 젠킨스 빌드 테스트

--- src/main/kotlin/com/flabedu/blackpostoffice/service/UserService.kt
+++ src/main/kotlin/com/flabedu/blackpostoffice/service/UserService.kt
@@ -1,74 +1,74 @@
-package com.flabedu.blackpostoffice.service
-
-import com.flabedu.blackpostoffice.commom.encryption.Sha256Encryption
-import com.flabedu.blackpostoffice.commom.utils.constants.JPEG
-import com.flabedu.blackpostoffice.commom.utils.constants.PNG
-import com.flabedu.blackpostoffice.controller.dto.UserInfoUpdateDto
-import com.flabedu.blackpostoffice.controller.dto.UserSignUpDto
-import com.flabedu.blackpostoffice.domain.mapper.UserMapper
-import com.flabedu.blackpostoffice.exception.DuplicateRequestException
-import com.flabedu.blackpostoffice.exception.FileRequestException
-import org.apache.tomcat.util.http.fileupload.impl.FileUploadIOException
-import org.springframework.stereotype.Service
-import org.springframework.transaction.annotation.Transactional
-import org.springframework.web.multipart.MultipartFile
-
-@Service
-class UserService(
-    private val userMapper: UserMapper,
-    private val sha256Encryption: Sha256Encryption,
-    private val amazonS3Service: AmazonS3Service,
-    private val sessionLoginService: SessionLoginService
-) {
-
-    @Transactional
-    fun saveUser(userSignUpDto: UserSignUpDto) {
-
-        duplicateEmailCheck(userSignUpDto.email)
-
-        userMapper.join(userSignUpDto.toUserEntity(sha256Encryption.encryption(userSignUpDto.password)))
-    }
-
-    @Transactional
-    fun userInfoUpdate(multipartFile: MultipartFile) = profileImageUpdate(multipartFile)
-
-    @Transactional
-    fun deleteProfileImage() {
-
-        val getCurrentUserEmail = sessionLoginService.getCurrentUserEmail()
-        val getMyProfileImage = userMapper.getProfileImage(getCurrentUserEmail)
-
-        if (getMyProfileImage != null) {
-            userMapper.updateNullProfileImage(getCurrentUserEmail)
-            amazonS3Service.deleteProfileImage(getMyProfileImage)
-        }
-    }
-
-    private fun profileImageUpdate(multipartFile: MultipartFile) {
-
-        imageTypeCheck(multipartFile)
-
-        val getCurrentUserEmail = sessionLoginService.getCurrentUserEmail()
-        val userInfoUpdateDto = UserInfoUpdateDto(multipartFile)
-        val uploadProfileImage = amazonS3Service.updateProfileImage(userInfoUpdateDto.profileImagePath)
-
-        deleteProfileImage()
-        userMapper.updateUserInfo(userInfoUpdateDto.toUserInfoUpdate(getCurrentUserEmail, uploadProfileImage))
-    }
-
-    private fun imageTypeCheck(multipartFile: MultipartFile) {
-
-        val imageType = multipartFile.contentType
-
-        if (imageType != PNG && imageType != JPEG) {
-            throw FileRequestException("JPEG 파일이나 PNG 파일만 업로드 하실 수 있습니다.")
-        }
-    }
-
-    private fun duplicateEmailCheck(email: String) {
-
-        if (userMapper.isExistsByEmail(email)) {
-            throw DuplicateRequestException("이미 존재하는 이메일 입니다.")
-        }
-    }
-}
+//package com.flabedu.blackpostoffice.service
+//
+//import com.flabedu.blackpostoffice.commom.encryption.Sha256Encryption
+//import com.flabedu.blackpostoffice.commom.utils.constants.JPEG
+//import com.flabedu.blackpostoffice.commom.utils.constants.PNG
+//import com.flabedu.blackpostoffice.controller.dto.UserInfoUpdateDto
+//import com.flabedu.blackpostoffice.controller.dto.UserSignUpDto
+//import com.flabedu.blackpostoffice.domain.mapper.UserMapper
+//import com.flabedu.blackpostoffice.exception.DuplicateRequestException
+//import com.flabedu.blackpostoffice.exception.FileRequestException
+//import org.apache.tomcat.util.http.fileupload.impl.FileUploadIOException
+//import org.springframework.stereotype.Service
+//import org.springframework.transaction.annotation.Transactional
+//import org.springframework.web.multipart.MultipartFile
+//
+//@Service
+//class UserService(
+//    private val userMapper: UserMapper,
+//    private val sha256Encryption: Sha256Encryption,
+//    private val amazonS3Service: AmazonS3Service,
+//    private val sessionLoginService: SessionLoginService
+//) {
+//
+//    @Transactional
+//    fun saveUser(userSignUpDto: UserSignUpDto) {
+//
+//        duplicateEmailCheck(userSignUpDto.email)
+//
+//        userMapper.join(userSignUpDto.toUserEntity(sha256Encryption.encryption(userSignUpDto.password)))
+//    }
+//
+//    @Transactional
+//    fun userInfoUpdate(multipartFile: MultipartFile) = profileImageUpdate(multipartFile)
+//
+//    @Transactional
+//    fun deleteProfileImage() {
+//
+//        val getCurrentUserEmail = sessionLoginService.getCurrentUserEmail()
+//        val getMyProfileImage = userMapper.getProfileImage(getCurrentUserEmail)
+//
+//        if (getMyProfileImage != null) {
+//            userMapper.updateNullProfileImage(getCurrentUserEmail)
+//            amazonS3Service.deleteProfileImage(getMyProfileImage)
+//        }
+//    }
+//
+//    private fun profileImageUpdate(multipartFile: MultipartFile) {
+//
+//        imageTypeCheck(multipartFile)
+//
+//        val getCurrentUserEmail = sessionLoginService.getCurrentUserEmail()
+//        val userInfoUpdateDto = UserInfoUpdateDto(multipartFile)
+//        val uploadProfileImage = amazonS3Service.updateProfileImage(userInfoUpdateDto.profileImagePath)
+//
+//        deleteProfileImage()
+//        userMapper.updateUserInfo(userInfoUpdateDto.toUserInfoUpdate(getCurrentUserEmail, uploadProfileImage))
+//    }
+//
+//    private fun imageTypeCheck(multipartFile: MultipartFile) {
+//
+//        val imageType = multipartFile.contentType
+//
+//        if (imageType != PNG && imageType != JPEG) {
+//            throw FileRequestException("JPEG 파일이나 PNG 파일만 업로드 하실 수 있습니다.")
+//        }
+//    }
+//
+//    private fun duplicateEmailCheck(email: String) {
+//
+//        if (userMapper.isExistsByEmail(email)) {
+//            throw DuplicateRequestException("이미 존재하는 이메일 입니다.")
+//        }
+//    }
+//}
