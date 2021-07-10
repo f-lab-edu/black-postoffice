3a385fb603a885b576d8167f66e1ecdf2aed4a4d
Hyung1Jung <junghyungile@gmail.com>
2021-07-11 05:05:52 +0900

    [#31] refactor : test

--- src/test/kotlin/com/flabedu/blackpostoffice/BlackPostofficeApplicationTests.kt
+++ src/test/kotlin/com/flabedu/blackpostoffice/BlackPostofficeApplicationTests.kt
@@ -1,9 +1,13 @@
 package com.flabedu.blackpostoffice
 
 import org.junit.jupiter.api.Test
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.TestPropertySource
 
 @SpringBootTest
+@ConfigurationPropertiesScan("com.flabedu.blackpostoffice.commom.property")
+@TestPropertySource("classpath:application-local.yml")
 class BlackPostofficeApplicationTests {
     @Test
     fun contextLoads() {
