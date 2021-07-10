575520b64466ef7fa2c7ad04423ce8d8306b4576
Hyung1Jung <junghyungile@gmail.com>
2021-07-11 04:37:56 +0900

    [#31] refactor : test

--- src/main/kotlin/com/flabedu/blackpostoffice/BlackPostofficeApplication.kt
+++ src/main/kotlin/com/flabedu/blackpostoffice/BlackPostofficeApplication.kt
@@ -1,13 +1,11 @@
 package com.flabedu.blackpostoffice
 
-import com.flabedu.blackpostoffice.commom.property.AwsProperties
-import com.flabedu.blackpostoffice.commom.property.RedisProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-@EnableConfigurationProperties(value = [AwsProperties::class, RedisProperties::class])
+@ConfigurationPropertiesScan("com.flabedu.blackpostoffice.commom.property")
 class BlackPostofficeApplication
 
 fun main(args: Array<String>) {
