4794b4b47ae23f30b8e3879fa45ba050a7fb7f3b
Hyung1Jung <junghyungile@gmail.com>
2021-07-11 04:49:00 +0900

    test

--- src/test/kotlin/com/flabedu/blackpostoffice/BlackPostofficeApplicationTests.kt
+++ src/test/kotlin/com/flabedu/blackpostoffice/BlackPostofficeApplicationTests.kt
@@ -1,15 +1,9 @@
 package com.flabedu.blackpostoffice
 
 import org.junit.jupiter.api.Test
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.TestConstructor
-import org.springframework.test.context.TestPropertySource
 
 @SpringBootTest
-@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
-@ConfigurationPropertiesScan("com.flabedu.blackpostoffice.commom.property")
-@TestPropertySource("classpath:application-local.yml")
 class BlackPostofficeApplicationTests {
     @Test
     fun contextLoads() {
