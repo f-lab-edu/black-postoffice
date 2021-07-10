8db54e86e7fac64a8c237024da7dbdafb1d37ad0
Hyung1Jung <junghyungile@gmail.com>
2021-07-11 04:38:48 +0900

    [#31] refactor : test

--- src/test/kotlin/com/flabedu/blackpostoffice/BlackPostofficeApplicationTests.kt
+++ src/test/kotlin/com/flabedu/blackpostoffice/BlackPostofficeApplicationTests.kt
@@ -1,6 +1,5 @@
 package com.flabedu.blackpostoffice
 
-
 import org.junit.jupiter.api.Test
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.test.context.SpringBootTest
