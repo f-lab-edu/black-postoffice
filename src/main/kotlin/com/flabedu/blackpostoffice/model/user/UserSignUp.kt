217c19c01c33c2b001272207683455280a798047
Hyung1Jung <junghyungile@gmail.com>
2021-07-29 21:33:55 +0900

    [#40] style : 코드 컨벤션 수정 (#41)
    Fixes #40
    develop 브렌치에 코드 컨벤션(Trailing space)이 어겨진 코드로 인해 ktlintCheck에 실패하여 젠킨스 빌드 실패 현상 발생
    
    상세 내용
    코드 컨벤션(Trailing space)수정하여 빌드 실패 현상 해결

--- src/main/kotlin/com/flabedu/blackpostoffice/model/user/UserSignUp.kt
+++ src/main/kotlin/com/flabedu/blackpostoffice/model/user/UserSignUp.kt
@@ -24,9 +24,7 @@ data class UserSignUp(
 
     @field:NotBlank(message = "휴대폰 번호는 필수 입력입니다.")
     @field:Pattern(regexp = "010-[0-9]{3,4}-[0-9]{4}", message = "올바른 휴대폰 번호를 입력해주세요.")
-    val phone: String,
-
-    val profileImagePath: String?,
+    val phone: String
 
 ) {
 
@@ -37,6 +35,5 @@ data class UserSignUp(
             nickName = nickName,
             address = address,
             phone = phone,
-            profileImagePath = profileImagePath
         )
 }
